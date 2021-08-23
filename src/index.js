e6dbfb4a4f0d62c101aed05d62f2031ab24ec59c
TatsiaA <taniokk@mail.ru>
2021-08-24 01:39:37 +0300

    fix: change code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,48 @@
 
-exports.min = function min (array) {
+exports.min = function min(array) {
+ if (array.length > 0 || array !== undefined) {
+   
+    let mini = array[0];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < mini) {
+     mini = array[i];
+    }  
+  } return mini;
+} else {
   return 0;
 }
+};
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  if (array.length > 0 || array !== undefined) {
+    
+let maxi = array[0];
+for (let i = 0; i < array.length; i++) {
+    if (array[i] > maxi) {
+        maxi = array[i];
+
+    }
 }
+return maxi;
+  } else {
+    return 0;
+  }
+   };
+
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array.length > 0 || array !== undefined) {
+    
+let avgr = 0,
+    sum = 0;
+for (let i = 0; i < array.length; i++) {
+    sum = sum + array[i];
 }
+avgr = sum / array.length;
+return avgr;
+  } else {
+    return 0;
+  
+  }
+
+};
